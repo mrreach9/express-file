198cd8d8ab6be98514709c740ae48a0d4d1fd936
ThireachHok <Hoksopheathireach18@kit.edu.kh>
2019-03-11 20:58:20 +0700

    made some changes

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get(`/`, (req, res) => {
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
 
 });
-app.listen(5000);
+app.listen(PORT, () => {
+    console.log(`server started on ${PORT}`);
+});
