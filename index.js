9e1921df171043d6b82e6e7a504c73e69af1b9c9
ThireachHok <Hoksopheathireach18@kit.edu.kh>
2019-03-11 21:08:18 +0700

    prepare for pushing

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+    // res.send('<h1>Hello World!!</h1>');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 });
 app.listen(PORT, () => {
