5c9b1f3f3dc33b282cc0f113b3f582f1aa5851c7
ThireachHok <Hoksopheathireach18@kit.edu.kh>
2019-03-11 21:17:01 +0700

    added some css and readfile

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-    // res.send('<h1>Hello World!!</h1>');
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-
-});
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
 });
