4c2719a387d7ef3b71e273865a01089131bf9137
ThireachHok <Hoksopheathireach18@kit.edu.kh>
2019-03-11 21:52:27 +0700

    api/members (loading JSON)

--- index.js
+++ index.js
@@ -3,9 +3,30 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const members = [{
+        name: 'reach',
+        age: 20
+    },
+    {
+        name: 'zreawwaach',
+        age: 20
+    },
+    {
+        name: 'qreacwwh',
+        age: 20
+    }
+];
+
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
 });
